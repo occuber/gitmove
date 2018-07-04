1861764b3a442e06ab5afb7456ec70345949a435
li.pengfeng <li.pengfeng@yanwei365.com>
2018-07-04 17:55:51 +0800

    测试提交

---
+++ app/src/main/java/com/coder/gitmove/MainActivity.java
@@ -0,0 +1,18 @@
+package com.coder.gitmove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    private void init(){
+        // 这是master数据
+    }
+}
